343073db7294b5de83e446d02bca2bccbe064858
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2024-07-17 17:14:41 -0500

    Update Inicio.java

--- formularios/Inicio.java
+++ formularios/Inicio.java
@@ -2,13 +2,11 @@ package formularios;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Inicio extends JDialog {
-    private JTextField tfuser;
+    private JTextField tfuser;               //Nombre de usuario
     private JButton entrarButton;
-    private JPasswordField pfpassword;
+    private JPasswordField pfpassword;       //Contraseña
     private JLabel usrName;
     private JLabel usrPw;
     private JPanel loginPanel;
@@ -19,24 +17,24 @@ public class Inicio extends JDialog {
         setTitle("Login");
         setContentPane(loginPanel);
         setMinimumSize(new Dimension(450, 450));
-        setModal(true);
+        setModal(true); //Hace la ventana modal
         setLocationRelativeTo(parent);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);  //Cierra los recursos al cerrar ventana
 
         entrarButton.addActionListener(e -> {
-            // Simplemente muestra la ventana Menu sin verificar usuario y contraseña
+            //TODO: Verificación de Usuario y contraseña para ingresar a Menu.
             Menu menu = new Menu(this);
             menu.setVisible(true);
             dispose();
         });
 
-        setVisible(true);
-        registrarUsuarioButton.addActionListener(e ->  {
-                //Botón que envía a Registro.java
-                Registro registro = new Registro(this);
-                registro.setVisible(true);
-                dispose();
+        registrarUsuarioButton.addActionListener(e -> {
+            // Botón que envía a Registro.java
+            Registro registro = new Registro(this);
+            registro.setVisible(true);
+            dispose();
         });
+
         setVisible(true);
     }
 
